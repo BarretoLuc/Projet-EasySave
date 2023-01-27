6dca0975f3bced314ac12be23e86b1f4d3451c19
valouugit <valentingoulier@gmail.com>
2023-01-27 09:37:43 +0100

    Ajout du model de fichier et du service de compute hash

---
+++ EasySaveConsole/Models/FileModel.cs
@@ -0,0 +1,51 @@
+﻿using EasySaveConsole.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.Models
+{
+    internal class FileModel
+    {
+        private string name;
+        private string path;
+        private string? hash;
+
+        public FileModel(
+            string name,
+            string path
+        )
+        {
+            this.name = name;
+            this.path = path;
+        }
+
+        public FileModel(
+            string fullPath
+        )
+        {
+            // Transform fullpath of file in path + name
+            string[] pathSplit = fullPath.Split('\\');
+            this.name = pathSplit[pathSplit.Length - 1];
+            this.path = fullPath.Replace(this.name, "");
+        }
+
+        public string FullPath
+        {
+            get
+            {
+                return this.path + this.name;
+            }
+        }
+
+        public string Hash
+        {
+            get
+            {
+                return (new HashService()).computeSHA256(this);
+            }
+        }
+    }
+}
--- EasySaveConsole/Program.cs
+++ EasySaveConsole/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EasySaveConsole.Models;
+using System.Security.Cryptography;
+
+// Walk through the list of files in the directory and print names of the files.
+string[] files = Directory.GetFiles(@"C:\share\Music");
+
+// Instance FileModel array with files array
+FileModel[] fileModels = new FileModel[files.Length];
+
+// Loop through files array
+for (int i = 0; i < files.Length; i++)
+{
+    // Create new FileModel with file path
+    fileModels[i] = new FileModel(files[i]);
+    // Print hash
+    Console.WriteLine(fileModels[i].Hash);
+}
