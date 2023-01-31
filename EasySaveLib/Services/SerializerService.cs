cc8ad609c844c4affe1231b81af86facbe037c71
valouugit <valentingoulier@gmail.com>
2023-01-31 14:53:27 +0100

    Ajout du service du serializer/deserializer

---
+++ EasySaveLib/Services/SerializerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveLib.Services
+{
+    internal class SerializerService
+    {
+        // Serialize another object to json native
+        public string Serialize<T>(T obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+
+        // Deserialize json native to another object
+        public T Deserialize<T>(string json)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
