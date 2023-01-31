f6715bb148252d4ffdbffa267dd6242e2a5cade0
BarretoLuc <barreto.luc.02@gmail.com>
2023-01-31 16:35:08 +0100

    Ajout du model job status.

---
+++ EasySaveLib/Models/StatusJobModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveLib.Models
+{
+    internal class StatusJobModel
+    {
+        public string Name { get; set; }
+        public string Timestamp { get; set; }
+        public bool Status { get; set; }
+        public int TotalFileNumber { get; set; }
+        public int TotalFileSize { get; set; }
+        public int RemainingFileNumber { get; set; }
+        public int RemainingFileSize { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        
+        public StatusJobModel(string name, string timestamp, bool status, int totalFileNumber, int totalFileSize, int remainingFileNumber, int remainingFileSize, string source, string destination)
+        {
+            Name = name;
+            Timestamp = timestamp;
+            Status = status;
+            TotalFileNumber = totalFileNumber;
+            TotalFileSize = totalFileSize;
+            RemainingFileNumber = remainingFileNumber;
+            RemainingFileSize = remainingFileSize;
+            Source = source;
+            Destination = destination;
+        }
+    }
+}
