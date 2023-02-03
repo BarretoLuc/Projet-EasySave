bed84a15c50485415064287fe9422c0b2a4f0f68
valouugit <valentingoulier@gmail.com>
2023-02-03 15:34:26 +0100

    Ajout du point d'entrée

--- EasySaveConsole/Program.cs
+++ EasySaveConsole/Program.cs
@@ -1,18 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EasySave.Models;
-using System.Security.Cryptography;
+﻿using EasySaveConsole.Vues;
+using System;
 
-// Walk through the list of files in the directory and print names of the files.
-string[] files = Directory.GetFiles(@"C:\share\Music");
-
-// Instance FileModel array with files array
-FileModel[] fileModels = new FileModel[files.Length];
-
-// Loop through files array
-for (int i = 0; i < files.Length; i++)
+namespace EasySaveConsole
 {
-    // Create new FileModel with file path
-    fileModels[i] = new FileModel(files[i]);
-    // Print hash
-    Console.WriteLine(fileModels[i].Hash);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Home viewHome = new Home();
+            viewHome.show();
+        }
+    }
 }
