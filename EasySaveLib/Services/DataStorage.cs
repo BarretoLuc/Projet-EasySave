fd5b122f3ac2b5047cb278fe5ceed206a61db76a
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-02-03 16:46:51 +0100

    initialisation class DataStorage

---
+++ EasySaveLib/Services/DataStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySaveLib.Models;
+
+namespace EasySaveLib.Services
+{
+    public class DataStorage
+    {
+        public List<JobModel>? JobList { get; }
+
+        public void addJobList() { }
+        public void removeJobList() { }
+    }
+}
