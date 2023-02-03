920594201b92155434ce799357020f127a0ef08d
valouugit <valentingoulier@gmail.com>
2023-02-03 15:50:59 +0100

    Ajout contructeur Home Console

--- EasySaveConsole/Vues/Home.cs
+++ EasySaveConsole/Vues/Home.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasySave.Controllers;
 using EasySave.Vues;
 
 namespace EasySaveConsole.Vues
 {
     internal class Home : IHome
     {
+        private HomeController HomeController { get; set; }
+        
+        public Home() {
+            HomeController = new HomeController();
+        }
+        
         public void show()
         {
             Console.WriteLine("Welcome to EasySave");
---
+++ EasySaveLib/Vues/IJobCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveLib.Vues
+{
+    internal interface IJobCreate
+    {
+        public void show();
+        public void newJob();
+    }
+}
