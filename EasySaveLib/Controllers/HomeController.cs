3146860a9ee56ee64c6de227523cce935b2493ba
valouugit <valentingoulier@gmail.com>
2023-02-06 11:21:25 +0100

    Ajout des contructeurs normalisés

--- EasySaveLib/Controllers/HomeController.cs
+++ EasySaveLib/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasySaveLib.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace EasySaveLib.Controllers
 {
     public class HomeController : AbstractController
     {
+        public new DataStorageService Storage { get; }
+
+        public HomeController()
+        {
+            Storage = new DataStorageService();
+        }
+        
     }
 }
--- EasySaveLib/Controllers/JobController.cs
+++ EasySaveLib/Controllers/JobController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EasySaveLib.Models;
+using EasySaveLib.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,18 @@ namespace EasySaveLib.Controllers
 {
     public class JobController : AbstractController
     {
+        public new DataStorageService Storage { get; }
+
+        public JobController(DataStorageService StorageService)
+        {
+            Storage = StorageService;
+        }
+
+        public void CreateJob(string name, string sourcepath, string destinationpath)
+        {
+            Storage.AddJobList(
+                new JobModel(name, sourcepath, destinationpath)
+            );
+        }
     }
 }
