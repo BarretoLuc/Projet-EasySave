97a4c1dcf6a3c4c918ca3ff84b026491178101b9
alex <alexandre.milhas@viacesi.fr>
2023-02-07 15:41:42 +0100

    Ajout de la vue JobView

---
+++ EasySaveConsole/Vues/JobView.cs
@@ -0,0 +1,38 @@
+﻿using EasySaveLib.Controllers;
+using EasySaveLib.Models;
+using EasySaveLib.Vues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveConsole.Vues
+{
+    internal class JobView : IJobView
+    {
+
+        public JobView()
+        {
+        }
+
+        public void show(JobModel job)
+        {
+            Console.WriteLine("Job's name :");
+            Console.WriteLine(job.Name);
+            Console.WriteLine("Job's source path :");
+            Console.WriteLine(job.Source);
+            Console.WriteLine("Job's destination path :");
+            Console.WriteLine(job.Destination);
+        }
+
+        public void showall(List<JobModel> listjob)
+        {
+            foreach(JobModel job in listjob)
+            {
+                show(job);
+            }
+        }
+
+    }
+}
---
+++ EasySaveLib/Vues/IJobView.cs
@@ -0,0 +1,19 @@
+﻿using EasySaveLib.Models;
+using EasySaveLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EasySaveLib.Vues
+{
+    public interface IJobView
+    {
+        public void show(JobModel job)
+        {
+
+        }
+    }
+}
