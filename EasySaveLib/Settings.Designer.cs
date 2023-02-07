6825d4af8d6a2eed34c368d4d32aa0ae9a5e6d25
valouugit <valentingoulier@gmail.com>
2023-02-07 10:22:07 +0100

    Line ident auto

--- EasySaveLib/Settings.Designer.cs
+++ EasySaveLib/Settings.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace EasySaveLib.Settings {
     
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
