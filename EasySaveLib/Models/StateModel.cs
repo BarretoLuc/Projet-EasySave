e62487c88bc7c694172e25654ed650fcbf311411
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-02-08 15:32:36 +0100

    Ajout Model Enum State

---
+++ EasySaveLib/Models/StateModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EasySaveLib.Models
+{
+    public enum State 
+    { 
+        Waiting, 
+        Finished
+    }
+}
