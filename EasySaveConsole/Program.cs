cc2bb5dbe2f29fea15ca1347ad9c0634a2f0ec80
valouugit <valentingoulier@gmail.com>
2023-02-10 14:28:26 +0100

    Remove old test

--- EasySaveConsole/Program.cs
+++ EasySaveConsole/Program.cs
@@ -1,19 +1,10 @@
 ﻿using EasySaveConsole.Vues;
 using EasySaveLib.Controllers;
-using System;
-using System.Globalization;
-using System.Reflection;
-using System.Resources;
-using System.Globalization;
-using System.ComponentModel;
 
 namespace EasySaveConsole
 {
     internal static class Program 
     {
-        static ResourceManager rm;
-        static CultureInfo cult;
-
         static Mutex mutex = new Mutex(true, "EasySave");
         [STAThread]
         static void Main(string[] args)
@@ -24,12 +15,6 @@ namespace EasySaveConsole
                 controller.init();
                 mutex.ReleaseMutex();
             }
-        }
-
-        static void switch_language()
-        {
-            cult = CultureInfo.CreateSpecificCulture("en");
-            rm = new ResourceManager("EasySaveConsole.Ressources.Languages.Res", Assembly.GetExecutingAssembly());
         }
     }
 }
