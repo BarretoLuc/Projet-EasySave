98e4f7c7659af8ad22f6b34e613b78d80699fee8
BarretoLuc <barreto.luc.02@gmail.com>
2023-02-16 13:53:21 +0100

    Ajout de la vue JobCreate.

--- EasySaveWPF/JobRunPage.xaml.cs
+++ EasySaveWPF/JobRunPage.xaml.cs
@@ -24,12 +24,15 @@ namespace EasySaveWPF
     public partial class JobRunPage : Page, IAbstractView<JobRunController>, IJobRun
     {
         public JobRunController Controller { get; set; }
+        private List<JobModel> ListJob;
         public JobRunPage()
         {
-            InitializeComponent();
         }
-        public void Show(List<JobModel> jobs)
+        public void Show(List<JobModel> listJob)
         {
+            InitializeComponent();
+            ListJob = listJob;
+            dgJob.ItemsSource = ListJob;
         }
         public void Progress(JobModel job)
         {
@@ -38,6 +41,22 @@ namespace EasySaveWPF
         {
             JobModel jobModel = new JobModel("","","");
             return jobModel;
+        }
+
+        private void ExecuteClick(object sender, RoutedEventArgs e)
+        {
+            if (dgJob.SelectedItems.Count <= 0)
+                return;
+            
+            if (dgJob.SelectedItems.Count == 1)
+                Controller.ExecuteOneJob(ListJob[dgJob.SelectedIndex]);
+            else if (dgJob.SelectedCells.Count > 1)
+            {
+                foreach (JobModel item in dgJob.SelectedItems)
+                {
+                    Controller.ExecuteOneJob(item);
+                }
+            }
         }
     }
 }
