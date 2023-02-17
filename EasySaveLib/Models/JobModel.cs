33c1cea8875c8a04d5492b0867e3e785ae0b8879
valouugit <valentingoulier@gmail.com>
2023-02-17 13:45:38 +0100

    Ajout des numberOfFiles

--- EasySaveLib/Models/JobModel.cs
+++ EasySaveLib/Models/JobModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StateFile = EasySaveLib.Models.State;
 
 namespace EasySaveLib.Models
 {
@@ -18,8 +19,9 @@ namespace EasySaveLib.Models
         [Newtonsoft.Json.JsonConverter(typeof(StringEnumConverter))]
         public JobStatsEnum? State { get; set; }
         public List<FileModel>? AllFiles { get; set; }
-        
-
+        public int NumberOfFiles { get => AllFiles?.Count ?? 0; }
+        public int NumberOfFilesRemaining { get => AllFiles?.Where(file => file.State != StateFile.Finished).Count() ?? 0; }
+        public int NumberOfFilesFinished { get => AllFiles?.Where(file => file.State == StateFile.Finished).Count() ?? 0; }
 
         public JobModel(
             string name,
